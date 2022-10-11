9ad20f6bfecccf3a3c58b1f5133e0ce30c3a1bfb
zebiLamouche93 <115323497+zebiLamouche93@users.noreply.github.com>
2022-10-11 11:33:24 +0200

    Add files via upload

---
+++ annuaire-VerEtudt/views/v_saisiemembre.php
@@ -0,0 +1,12 @@
+
+ 
+    <form action= "index.php?ac=gerer&action=ctrlsaisir" method="post">
+
+        nom :<input type="text" name="nom">
+        prenom :<input type="text" name="prenom">
+        <input type="submit" value="valider">
+        <input type="submit" value="annuler">
+        
+        </form>
+
+       
